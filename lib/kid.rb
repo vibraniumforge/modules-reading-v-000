faa336fd88d8db520fcdc0f7299b39ee37edcac3
Greg Morgan <gregm9999@gmail.com>
2018-11-13 23:20:37 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,18 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-require_relative './fancy_dance.rb'
+
 
 class Kid
   include Dance
-
+  extend MetaDancing
   attr_accessor :name
 
 def initialize(name)
   @name = name
 end
 
-end
-
-class Kid
-  extend MetaDancing
 end
